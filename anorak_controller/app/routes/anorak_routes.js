dba30bb72f919cb3446d85e442912ced04fab61e
Brede Børhaug <brede.borhaug@dnvgl.com>
2019-01-31 12:12:58 +0100

    new architecture

--- anorak_controller/app/routes/anorak_routes.js
+++
@@ -1,7 +0,0 @@
-// anorak.js
-module.exports = function(app, db) {
-    app.post('/anorak', (req, res) => {      
-      console.log(req.body)
-      res.send('Hello from Anorak')
-    });
-  };
--- anorak_controller/app/routes/echo_routes.js
+++
@@ -1,7 +0,0 @@
-// anorak.js
-module.exports = function(app) {
-    app.post('/echo', (req, res) => {      
-      console.log(req.body)
-      res.send(req.body)
-    });
-  };
